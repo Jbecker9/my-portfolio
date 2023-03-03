69f602ab90a56c061ce7ed9c369fbd6db27718a4
Jared <jbeckersalax9@aol.com>
2023-03-03 10:39:16 -0500

    github link added to memoroutes

--- src/components/Body/BodyContents/Projects.jsx
+++ src/components/Body/BodyContents/Projects.jsx
@@ -7,10 +7,10 @@ function Projects(){
         <div>
             <div className="Projects-Memoroutes-Div">
                 <h2 className="Projects-Memoroutes-Title">Memoroutes</h2>
+                <a href="https://github.com/Jbecker9/memoroutes">
+                    <i id="Projects-Memoroutes-Github-Link"className="fa-brands fa-square-github fa-inverse fa-2x" />
+                </a>
                 <img className="Projects-Memoroutes-Img" src="https://i.imgur.com/Duk0D7p.png"/>
-                <div>
-                <i className="fa-brands fa-square-github fa-inverse fa-2x" href="https://github.com/Jbecker9/memoroutes"></i>
-                </div>
             </div>
         </div>
     )
